49596f44e238a9b40f6feb49a94316166cbbe0af
Daniel <69382818+argoz1701@users.noreply.github.com>
2022-06-28 21:39:22 -0400

    Update RemoteMailbox.ps1
    Formatting update

--- RemoteMailbox.ps1
+++ RemoteMailbox.ps1
@@ -4,40 +4,40 @@
 
 While ($true) {
 
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 
-
-                Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
-
-
-
+                
                 Write-Output "Enter Username then Email. Type Ctrl+C to Exit.`n"
-
-
-
+                
                 $userName = Read-Host -Prompt "Enter the user name"
-
-
-
                 $userEmail = Read-Host -Prompt "Enter email"
+                
 
-
-
-                                Enable-MailUser -Identity $userName –ExternalEmailAddress $userName@domain.mail.onmicrosoft.com
-
-
-
-                                Enable-RemoteMailbox $userEmail@domain.com
-
-
-
-                                Set-ADUser -Identity $userName -Replace @{msExchUserAccountControl="0"} # 0 = enable, 2 = disable
-
-
+                           Enable-MailUser -Identity $userName –ExternalEmailAddress $userName@domain.mail.onmicrosoft.com
+                           Enable-RemoteMailbox $userEmail@domain.com
+                           Set-ADUser -Identity $userName -Replace @{msExchUserAccountControl="0"} # 0 = enable, 2 = disable
 
                 If ($condition -eq $true) {
-
-                                Break
+                          Break
 
                 }
 
-}
+}                                
+
+
+
+                               
+
+
+
+                               
+
+
+
+                
+
+                               
+
+                
+
+
