93a4ba7a9e2bf6285bd474153641496fbbd1dca9
Daniel <69382818+argoz1701@users.noreply.github.com>
2022-06-28 21:33:42 -0400

    PowerShell script

---
+++ RemoteMailbox.ps1
@@ -0,0 +1,43 @@
+ #Enter log in name and email <name.last>
+
+
+
+While ($true) {
+
+
+
+                Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+
+
+                Write-Output "Enter Username then Email. Type Ctrl+C to Exit.`n"
+
+
+
+                $userName = Read-Host -Prompt "Enter the user name"
+
+
+
+                $userEmail = Read-Host -Prompt "Enter email"
+
+
+
+                                Enable-MailUser -Identity $userName –ExternalEmailAddress $userName@domain.mail.onmicrosoft.com
+
+
+
+                                Enable-RemoteMailbox $userEmail@domain.com
+
+
+
+                                Set-ADUser -Identity $userName -Replace @{msExchUserAccountControl="0"} # 0 = enable, 2 = disable
+
+
+
+                If ($condition -eq $true) {
+
+                                Break
+
+                }
+
+}
